d53bf39d06ee78e52a15bf2499b743d202828fdd
Nyaikamba <dmbzb@mail.umkc.edu>
2020-05-14 07:31:24 -0500

    Added story sort

---
+++ README.md
@@ -0,0 +1,2 @@
+# HackerNews Scrapper 
+This goes through the HackerNews Website and grabs stories with points greater than 100
--- scraper.py
+++ scraper.py
@@ -1,16 +1,29 @@
+# libraries required
+
 import requests
 import pprint
 from bs4 import BeautifulSoup
 
-res = requests.get('https://news.ycombinator.com/')
-# print(res.text)
 
-hacker_soup = BeautifulSoup(res.text, 'html.parser')
+# Get data by looping through first 5 pages on HackerNews
+
+def get_data():
+    for i in range(1,5):
+        res = requests.get('https://news.ycombinator.com/news?p={page}'.format(page =i))
+        hacker_soup = BeautifulSoup(res.text, 'html.parser')
+        links = hacker_soup.select('.storylink')
+        subtext = hacker_soup.select('.subtext')
+
+        return custom_hacker_news(links, subtext)
 
 
-links = hacker_soup.select('.storylink')
-subtext = hacker_soup.select('.subtext')
+# Sort stories with highest ranked coming first
 
+def sort_by_points(news_list):
+    return sorted(news_list, key=lambda k: k['points'], reverse=True)
+
+
+# Get data into a list, only add stories with more than 100 points
 
 def custom_hacker_news(links, subtext):
     hacker_news = []
@@ -21,10 +34,10 @@ def custom_hacker_news(links, subtext):
 
         if len(news_votes):
             points = int(news_votes[0].getText().replace(' points', ' '))
-            if (points>100):
+            if (points > 99):
                 hacker_news.append({'title': news_title, 'link': news_link, 'points': points})
 
-    return hacker_news
+    return sort_by_points(hacker_news)
 
 
-pprint.pprint(custom_hacker_news(links, subtext))
+pprint.pprint(get_data())
